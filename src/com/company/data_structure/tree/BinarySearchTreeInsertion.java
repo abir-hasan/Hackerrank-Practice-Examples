01d01b2af57c5213fdd88273b3b45c2b9e295e0a
abir-hasan <abirhasan89@gmail.com>
2017-05-24 18:22:41 +0600

    Solved: Data Structures>Trees>Binary Search Tree : Insertion

---
+++ src/com/company/data_structure/tree/BinarySearchTreeInsertion.java
@@ -0,0 +1,65 @@
+package com.company.data_structure.tree;
+
+/**
+ * Created by Abir on 24-May-17.
+ */
+public class BinarySearchTreeInsertion {
+
+    public static void main(String[] args) {
+        TreeNode node = Insert(createDummyTree(), 0);
+        inOrder(node); // Print
+    }
+
+    static TreeNode createDummyTree() {
+        TreeNode node1 = new TreeNode();
+        node1.left = null;
+        node1.right = null;
+        node1.data = 1;
+
+        TreeNode node3 = new TreeNode();
+        node3.left = null;
+        node3.right = null;
+        node3.data = 3;
+
+        TreeNode node2 = new TreeNode(node1, node3, 2);
+        TreeNode node5 = new TreeNode(null, null, 5);
+        TreeNode node4 = new TreeNode(node2, node5, 4);
+        return node4;
+    }
+
+    // Assuming no duplicate entries
+    static TreeNode Insert(TreeNode root, int value) {
+        TreeNode originalRoot = root;
+        TreeNode newNode = new TreeNode();
+        newNode.left = null;
+        newNode.right = null;
+        newNode.data = value;
+        if (originalRoot == null) return newNode;
+
+        while (root != null) {
+            if (root.data > value) {
+                if (root.left != null) {
+                    root = root.left;
+                } else {
+                    root.left = newNode;
+                    break;
+                }
+            } else {
+                if (root.right != null) {
+                    root = root.right;
+                } else {
+                    root.right = newNode;
+                    break;
+                }
+            }
+        }
+        return originalRoot;
+    }
+
+    static void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+}
--- src/com/company/data_structure/tree/TreeNode.java
+++ src/com/company/data_structure/tree/TreeNode.java
@@ -4,4 +4,17 @@ package com.company.data_structure.tree;
  * Created by Abir on 24-May-17.
  */
 public class TreeNode {
+    public TreeNode left;
+    public TreeNode right;
+    public int data;
+
+    public TreeNode() {
+
+    }
+
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+    }
 }
