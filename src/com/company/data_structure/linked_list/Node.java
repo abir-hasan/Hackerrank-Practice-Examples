12e6e6da69fc448bc599566fb5a408522df49bef
Abir Hasan <abirhasan89@gmail.com>
2017-05-25 18:05:48 +0600

    refactoring

--- src/com/company/data_structure/linked_list/Node.java
+++ src/com/company/data_structure/linked_list/Node.java
@@ -4,7 +4,7 @@ package com.company.data_structure.linked_list;
  * Created by Abir Hasan on 25-May-17.
  */
 
-public class Node {
+class Node {
     int data;
     Node next;
 }
--- src/com/company/data_structure/tree/TreeNode.java
+++ src/com/company/data_structure/tree/TreeNode.java
@@ -3,16 +3,16 @@ package com.company.data_structure.tree;
 /**
  * Created by Abir on 24-May-17.
  */
-public class TreeNode {
-    public TreeNode left;
-    public TreeNode right;
-    public int data;
+class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int data;
 
-    public TreeNode() {
+    TreeNode() {
 
     }
 
-    public TreeNode(TreeNode left, TreeNode right, int data) {
+    TreeNode(TreeNode left, TreeNode right, int data) {
         this.left = left;
         this.right = right;
         this.data = data;
