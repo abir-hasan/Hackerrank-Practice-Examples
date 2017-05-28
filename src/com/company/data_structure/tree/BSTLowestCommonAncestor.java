e5a5148acb14f37d7be3d9db1e1892fee923851e
abir-hasan <abirhasan89@gmail.com>
2017-05-28 23:35:31 +0600

    added class for LCA

---
+++ src/com/company/data_structure/tree/BSTLowestCommonAncestor.java
@@ -0,0 +1,16 @@
+package com.company.data_structure.tree;
+
+/**
+ * Created by Abir on 28-May-17.
+ * Problem: Data Structures >Trees >Binary Search Tree : Lowest Common Ancestor
+ */
+public class BSTLowestCommonAncestor {
+
+    public static void main(String[] args) {
+        TreeNode bstRoot = BinarySearchTreeInsertion.createDummyTree();
+    }
+
+    static TreeNode lca(TreeNode root, int v1, int v2) {
+        return root;
+    }
+}
