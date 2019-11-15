9efb2615eae86861abb73a5ff47f39f6ac6581ae
Abir <abirhasan89@gmail.com>
2019-11-15 21:20:36 +0600

    If-Else problem solved (Java)

---
+++ src/com/company/language_java/PracticeIfElse.java
@@ -0,0 +1,28 @@
+package com.company.language_java;
+
+import java.util.Scanner;
+
+/**
+ * Created By Abir Hasan on 15-Nov-19
+ * Practice > Java > Introduction > Java If-Else
+ */
+public class PracticeIfElse {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+
+        if (value % 2 != 0) {
+            System.out.println("Weird");
+        } else {
+            if (value <= 5) {
+                System.out.println("Not Weird");
+            } else if (value <= 20) {
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+        scanner.close();
+    }
+}
