a07ddb15de202ebfe7fd15eb6bdbd1404d20064f
Abir <abirhasan89@gmail.com>
2019-11-24 22:45:22 +0600

    Java Currency Formatter problem solved (Java)

---
+++ src/com/company/language_java/CurrencyFormatter.java
@@ -0,0 +1,37 @@
+package com.company.language_java;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Created By Abir Hasan on 24-Nov-19
+ *
+ * Problem: Practice > Java > Introduction > Java Currency Formatter
+ */
+public class CurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+
+        NumberFormat nfUs = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = nfUs.format(payment);
+
+        NumberFormat nfCh = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = nfCh.format(payment);
+
+        NumberFormat nfFr = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = nfFr.format(payment);
+
+
+        String india = us.replace("$", "Rs.");
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
