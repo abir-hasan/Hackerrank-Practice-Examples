47bdf1d2c0b5790cd439836cc0cf733ebee2b179
Abir <abirhasan89@gmail.com>
2019-11-24 23:01:17 +0600

    Java Syntax Checker problem solved (Java)

---
+++ src/com/company/language_java/SyntaxChecker.java
@@ -0,0 +1,27 @@
+package com.company.language_java;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Created By Abir Hasan on 24-Nov-19
+ * <p>
+ * Problem: Practice > Java > Introduction > Java Syntax Checker
+ */
+public class SyntaxChecker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            try {
+                Pattern pat = Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException ex) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+}
