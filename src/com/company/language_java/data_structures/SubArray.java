606503631d32f48d0e8598287d4f35bf80776f8e
Abir <abirhasan89@gmail.com>
2019-12-27 19:40:33 +0600

    Java Sub-array problem solved (Java)
    - added an example as comment

--- src/com/company/language_java/data_structures/SubArray.java
+++ src/com/company/language_java/data_structures/SubArray.java
@@ -9,6 +9,15 @@ import java.util.Scanner;
  * Practice > Java > Data Structures > Java Subarray
  *
  * @see: <a href="Link">https://www.hackerrank.com/challenges/java-negative-subarray/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen</a>
+ * <p>
+ * Sample Input
+ * <p>
+ * 5
+ * 1 -2 4 -5 1
+ * <p>
+ * Sample Output
+ * <p>
+ * 9
  */
 public class SubArray {
 
@@ -29,6 +38,12 @@ public class SubArray {
         // Like in first step there will be one array of n digits
         // In second step there will be two array of n-1 digits
         // In third step there will be three array of n-2 digits, so on and so forth...
+        // For example say, there are 5 digits in an array
+        // In first step there will be 1 array of 5 digits
+        // In second step there will be 2 array of 4 digits
+        // In third step there will be 3 array of 3 digits
+        // In fourth step there will be 4 array of 2 digits
+        // In fifth step there will be 5 array of 1 digits
         for (int i = 1; i <= n; i++) {
             int noOfDigitsInSubArray = n - i + 1;
             for (int j = 0; j < i; j++) {
